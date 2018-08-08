6e33feccb9a1fca573fbf60fdf86dc840097c3cc
Miguel Hiciano <mikewashere@outlook.es>
2018-08-08 02:19:07 -0400

    fixed

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function reloj()
     var hora = fecha.getHours();
     var minuto = fecha.getMinutes();
     var segundo = fecha.getSeconds();
+    
     if(segundo < 10 && minuto < 10 && hora < 10){
         var time = "0" + hora + ":0" + minuto + ":0" + segundo;
     }
@@ -15,19 +16,20 @@ function reloj()
     else if(segundo < 10 && hora < 10){
         var time = "0" + hora + ":" + minuto + ":0" + segundo;
     }
-    
+    else if(minuto < 10 && hora < 10){
+        var time = "0" + hora + ":0" + minuto + ":" + segundo;
+    }
     else if(segundo < 10){
         var time = hora + ":" + minuto + ":0" + segundo;
     }
-
     else if(minuto < 10){
         var time = hora + ":0" + minuto + ":" + segundo;
     }
-
     else if(hora < 10){
         var time ="0" + hora + ":" + minuto + ":" + segundo;
     }
     clock.innerHTML = time;
 }
+
 reloj();
 setInterval(reloj,1000);
